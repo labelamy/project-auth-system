87a327fbb926bbfe8bce05489974b30e1c6c4b28
komi <clientmabanquesio2@gmail.com>
2026-04-23 14:27:45 +0200

    modification de l'enregistrement

--- register.php
+++ register.php
@@ -1,7 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include "db.php";
 
-if ($_POST) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -13,3 +16,28 @@ if ($_POST) {
     echo "Utilisateur créé avec succès";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Enregistrer</title>
+</head>
+<body>
+
+<h2>Inscription</h2>
+
+<form method="POST">
+    <label>Nom d'utilisateur</label><br>
+    <input type="text" name="username" required>
+    <br><br>
+
+    <label>Mot de passe</label><br>
+    <input type="password" name="password" required>
+    <br><br>
+
+    <button type="submit">S'inscrire</button>
+</form>
+
+</body>
+</html>
